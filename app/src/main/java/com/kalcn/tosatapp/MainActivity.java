70cba987e3e1139b420de7293ea471d8275d89d5
Kalcn <zoon3312@gmail.com>
2020-05-25 11:56:11 +0800

    第一次更新程式碼

--- app/src/main/java/com/kalcn/tosatapp/MainActivity.java
+++ app/src/main/java/com/kalcn/tosatapp/MainActivity.java
@@ -2,12 +2,40 @@ package com.kalcn.tosatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int num1,num2,numSum;
+    private TextView txvNum1,txvNum2,txvAns,txvMyAns;
+    private EditText edtMyans;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+        resetNumber();
     }
+
+    private void findViews(){
+        txvNum1=findViewById(R.id.tevNum1);
+        txvNum2=findViewById(R.id.tevNum2);
+        txvAns=findViewById(R.id.tevAns);
+        txvMyAns=findViewById(R.id.tevMyAns);
+        edtMyans=findViewById(R.id.edtMyans);
+    }
+
+    private void resetNumber(){
+        num1 = (int)(Math.random()*10);
+        num2 = (int)(Math.random()*10);
+        txvNum1.setText(String.valueOf(num1));
+        txvNum2.setText(String.valueOf(num2));
+    }
+
+    
+
 }
